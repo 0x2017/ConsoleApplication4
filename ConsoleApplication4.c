7d28857a6e8e09f9e1ad81d749849defaa208d52
｡◕‿◕｡ <crl@hush.ai>
2017-04-23 13:50:14 +0900

    Add files via upload

---
+++ ConsoleApplication4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	float x;
+
+	printf("실수를 입력하시오: ");
+	scanf_s("%f", &x);
+	printf("실수형식으로는 %f 입니다.\n", x);
+	printf("지수형식으로는 %e 입니다.\n", x);
+
+	return 0;
+
+}
